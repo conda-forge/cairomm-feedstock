1e1db25476d1ed0ffa1cefa69757a54ca2320ffb
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-08-28 07:50:09 +0530

    Disable subproject fallback

--- recipe/build_cairomm.sh
+++ recipe/build_cairomm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-meson ${MESON_ARGS} --prefix="${PREFIX}" -Dlibdir=lib builddir .
+meson ${MESON_ARGS} --wrap-mode=nofallback --prefix="${PREFIX}" -Dlibdir=lib builddir .
 
 ninja -C builddir -j${CPU_COUNT}
 ninja -C builddir test
